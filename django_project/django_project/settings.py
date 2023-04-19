31085eb5c5d0485247388c9767de596a6c663ea8
Jark6 <enter_y@e1.ru>
2023-04-19 11:39:03 +0500

    authorisation form complete

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = "home" # new
+
+LOGOUT_REDIRECT_URL = "home" # new
